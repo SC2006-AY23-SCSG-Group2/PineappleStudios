b391c60aa4cd0e58bb0b8d356a600e035900943f
HUI Xujia <XHUI001@e.ntu.edu.sg>
2024-02-16 17:11:15 +0900

    update index

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -12,7 +12,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{fontFamily: "system-ui, sans-serif", lineHeight: "1.8"}}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Login</h1>
       <TestComponent />
     </div>
   );
