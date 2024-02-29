aa0d94c1f52aab8e03d1172577a94bd6bbb3efaa
BobH <dev@mail.bobh.cool>
2024-02-29 22:40:23 +0900

    clean tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,5 @@
 import type {Config} from "tailwindcss";
 
-const defaultTheme = require("tailwindcss/defaultTheme");
-// import {theme} from "tailwindcss/defaultTheme"
-
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
