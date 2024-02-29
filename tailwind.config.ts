ebdfc946ea1c09747e06151f6b80a257af54c896
BobH <dev@mail.bobh.cool>
2024-02-29 22:29:46 +0900

    Update the width of textfield

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,12 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      xs: "360px",
-      ...defaultTheme.screens,
+      "xs": "360px",
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px",
     },
   },
   plugins: [require("daisyui")],
