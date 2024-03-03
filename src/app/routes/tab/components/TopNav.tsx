45c3389b5253dc929cd28c5fcbfd47132722003b
BobH <dev@mail.bobh.cool>
2024-03-03 20:46:39 +0900

    Update tab layout: change button/a into NavLink

--- src/app/routes/tab/components/TopNav.tsx
+++ src/app/routes/tab/components/TopNav.tsx
@@ -1,10 +1,7 @@
+import {NavLink} from "@remix-run/react";
 import React from "react";
 
-export default function TopNav({
-  activate,
-}: {
-  activate: string;
-}): React.JSX.Element {
+export default function TopNav(): React.JSX.Element {
   return (
     <>
       <nav className="navbar sticky top-0 z-40 bg-base-100 max-lg:hidden lg:visible">
@@ -15,35 +12,30 @@ export default function TopNav({
         </div>
         <div className="navbar-center lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <a
-                className={activate === "1" ? "btn-active" : ""}
-                href={"/tab/1"}>
-                Item 1
-              </a>
+            <li className="menu-item px-2">
+              <NavLink to={"/tab/1"}>Item 1</NavLink>
             </li>
-            <li>
-              <a className={activate === "2" ? "btn-active" : ""} href={"/tab"}>
-                Item 2
-              </a>
+            <li className="menu-item px-2">
+              <NavLink to={"/tab/2"}>Item 2</NavLink>
             </li>
-            <li>
-              <a className={activate === "3" ? "btn-active" : ""} href={"/tab"}>
-                Item 3
-              </a>
+            <li className="menu-item px-2">
+              <NavLink to={"/tab/3"}>Item 3</NavLink>
             </li>
           </ul>
         </div>
         <div className="navbar-end">
           {/*<ThemeChanger />*/}
-          <div role="button" className="btn btn-ghost btn-circle avatar">
+          <NavLink
+            to={"/tab/4"}
+            role="button"
+            className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
               <img
                 alt="Tailwind CSS Navbar component"
                 src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
               />
             </div>
-          </div>
+          </NavLink>
         </div>
       </nav>
     </>
--- src/app/routes/tab/route.tsx
+++ src/app/routes/tab/route.tsx
@@ -1,15 +1,35 @@
-import {Outlet} from "@remix-run/react";
+import {LoaderFunctionArgs, json} from "@remix-run/node";
+import {Outlet, useLoaderData} from "@remix-run/react";
 import React from "react";
 
 import BtmNav from "./components/BtmNav";
 import TopNav from "./components/TopNav";
 
+export async function loader({request}: LoaderFunctionArgs) {
+  // parse the search params for `?q=`
+  const url = new URL(request.url);
+  let index = url.href
+    .split("/")
+    .filter((e) => {
+      if (e !== "/") {
+        return e;
+      }
+    })
+    .pop();
+  if (index === undefined || index === null || index === "tab") {
+    index = "1";
+  }
+  return json({index: index});
+}
+
 export default function tab(): React.JSX.Element {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const data = useLoaderData<typeof loader>();
   return (
     <>
-      <TopNav activate={"1"} />
+      <TopNav activate={data.index} />
       <Outlet />
-      <BtmNav />
+      <BtmNav activate={data.index} />
     </>
   );
 }
