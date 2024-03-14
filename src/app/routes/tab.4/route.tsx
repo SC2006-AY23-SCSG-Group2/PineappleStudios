9d6c4f5693cf22336a12d188fcb70649b1143973
Rudra Prasadh Ganesh <grudraprasadh@gmail.com>
2024-03-14 17:26:56 +0800

    Working on user profile page

--- src/app/routes/tab.4/route.tsx
+++ src/app/routes/tab.4/route.tsx
@@ -1,49 +1,45 @@
+import { useLoaderData } from "@remix-run/react";
 import React from "react";
 
+export const loader = async () => {
+  return {
+    user: {
+      name: "Unknown",
+      email: "unknown@e.ntu.edu.sg",
+    },
+  };
+};
+
 export default function tab_index(): React.JSX.Element {
+  const { user } = useLoaderData<typeof loader>();
+
   return (
-    <>
-      <p>aaaa</p>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+    <div className="flex">
+      <div className="card w-96 bg-base-100 shadow-xl m-4">
+        <div className="flex">
+          <div className="avatar online">
+            <div className="w-24 rounded-full">
+              <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </div>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
+    </div>
+    <div className="card w-full bg-base-100 shadow-xl my-4">
+      <div className="card-body">
+        <h2 className="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Buy Now</button>
         </div>
       </div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
-      <p>aaaa</p>
-    </>
+    </div>
+  </div>
   );
 }
