54083220944ada87279c5d1b64c1b3b5915d2dfc
Rudra Prasadh Ganesh <grudraprasadh@gmail.com>
2024-03-21 00:41:55 +0800

    Created usable components, General settings page

---
+++ src/app/routes/settings.general/route.tsx
@@ -0,0 +1,194 @@
+import React from "react";
+
+export default function tab_index(): React.JSX.Element {
+  return (
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap"
+        rel="stylesheet"
+      />
+
+      <div className="mx-4 min-h-screen max-w-screen-xl lg:mx-8 xl:mx-auto">
+        <h1 className="flex flex-row items-center border-b py-6 text-4xl font-semibold">
+          <div className="mr-4 flex h-full items-center justify-center">
+            <svg
+              className="align-center"
+              xmlns="http://www.w3.org/2000/svg"
+              width="35"
+              height="35"
+              viewBox="0 0 512 512">
+              <path
+                d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"
+                fill="blue"
+              />
+            </svg>
+          </div>
+          Settings
+        </h1>
+        <div className="grid grid-cols-8 pt-3 lg:grid-cols-10">
+          <div className="relative my-4 w-56 lg:hidden">
+            <input
+              className="peer hidden"
+              type="checkbox"
+              name="select-1"
+              id="select-1"
+            />
+            <label
+              htmlFor="select-1"
+              className="flex w-full cursor-pointer select-none rounded-lg border p-2 px-3 text-sm text-gray-700 ring-blue-700 peer-checked:ring">
+              Accounts{" "}
+            </label>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="pointer-events-none absolute right-0 top-3 ml-auto mr-5 h-4 text-slate-700 transition peer-checked:rotate-180"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+            <ul className="max-h-0 select-none flex-col overflow-hidden rounded-b-lg shadow-md transition-all duration-300 peer-checked:max-h-56 peer-checked:py-3">
+              <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-blue-700 hover:text-white">
+                Accounts
+              </li>
+              <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-blue-700 hover:text-white">
+                Team
+              </li>
+              <li className="cursor-pointer px-3 py-2 text-sm text-slate-600 hover:bg-blue-700 hover:text-white">
+                Others
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-span-2 hidden lg:block">
+            <ul>
+              <li className="mt-5 cursor-pointer border-l-2 border-l-blue-700 px-2 py-2 font-semibold text-blue-700 transition hover:border-l-blue-700 hover:text-blue-700">
+                Account
+              </li>
+              <li className="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-blue-700 hover:text-blue-700">
+                Profile
+              </li>
+              <li className="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-blue-700 hover:text-blue-700">
+                Notifications
+              </li>
+              <li className="mt-5 cursor-pointer border-l-2 border-transparent px-2 py-2 font-semibold transition hover:border-l-blue-700 hover:text-blue-700">
+                Log Out
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-span-8 overflow-hidden rounded-xl lg:bg-gray-50 lg:px-8 lg:shadow">
+            <div className="pt-4">
+              <h1 className="py-2 text-2xl font-semibold">Account settings</h1>
+            </div>
+            <hr className="mb-8 mt-4" />
+            <p className="py-2 text-xl font-semibold">Language</p>
+            <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between">
+              <p className="text-gray-600">
+                <strong>English</strong>
+              </p>
+              <button className="inline-flex text-sm font-semibold text-blue-600 underline decoration-2">
+                Change
+              </button>
+            </div>
+            <hr className="mb-8 mt-4" />
+            <p className="py-2 text-xl font-semibold">Email Address</p>
+            <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between">
+              <p className="text-gray-600">
+                Your email address is <strong>john.doe@company.com</strong>
+              </p>
+              <button className="inline-flex text-sm font-semibold text-blue-600 underline decoration-2">
+                Change
+              </button>
+            </div>
+            <hr className="mb-8 mt-4" />
+            <p className="py-2 text-xl font-semibold">Password</p>
+            <div className="flex items-center">
+              <div className="flex flex-col space-y-2 lg:flex-row lg:space-x-3 lg:space-y-0">
+                <label htmlFor="login-password">
+                  <span className="text-sm text-gray-500">
+                    Current Password
+                  </span>
+                  <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+                    <input
+                      type="password"
+                      id="login-password"
+                      className="w-full flex-shrink appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                      placeholder="***********"
+                    />
+                  </div>
+                </label>
+                <label htmlFor="login-password">
+                  <span className="text-sm text-gray-500">New Password</span>
+                  <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+                    <input
+                      type="password"
+                      id="login-password"
+                      className="w-full flex-shrink appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                      placeholder="***********"
+                    />
+                  </div>
+                </label>
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="ml-2 mt-5 h-6 w-6 cursor-pointer text-sm font-semibold text-gray-600 underline decoration-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                />
+              </svg>
+            </div>
+            <p className="mt-2">
+              Can't remember your current password.{" "}
+              <a
+                className="text-sm font-semibold text-blue-600 underline decoration-2"
+                href="#">
+                Recover Account
+              </a>
+            </p>
+            <button className="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white">
+              Save Password
+            </button>
+            <hr className="mb-8 mt-4" />
+
+            <div className="mb-10">
+              <p className="py-2 text-xl font-semibold">Delete Account</p>
+              <p className="inline-flex items-center rounded-full bg-rose-100 px-4 py-1 text-rose-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="mr-2 h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path
+                    fill-rule="evenodd"
+                    d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                Proceed with caution
+              </p>
+              <p className="mt-2">
+                Make sure you have taken backup of your account in case you ever
+                need to get access to your data. We will completely wipe your
+                data. There is no way to access your account after this action.
+              </p>
+              <button className="ml-auto text-sm font-semibold text-rose-600 underline decoration-2">
+                Continue with deletion
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
