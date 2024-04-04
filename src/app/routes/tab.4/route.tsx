56e2a6d954cc72fa1009f205b3c3152e45775bb5
BobH <dev@mail.bobh.cool>
2024-04-05 00:46:05 +0900

    <update>[tab.4]: update tab 4 layout to fit mobile views

--- src/app/routes/tab.4/route.tsx
+++ src/app/routes/tab.4/route.tsx
@@ -3,12 +3,34 @@ import {useLoaderData} from "@remix-run/react";
 import React from "react";
 import {getSession} from "src/app/session";
 
+import {SimpleItem} from "../../../lib/interfaces";
 import {TagList} from "../_components/TagList";
 import {HistoryItemList} from "./components/HistoryItemList";
 import {UserProfileCard} from "./components/UserProfileCard";
 
 export async function loader({request}: LoaderFunctionArgs) {
   const session = await getSession(request.headers.get("cookie"));
+
+  function randomInteger(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function makeItems(): SimpleItem[] {
+    const returnList: SimpleItem[] = [];
+    for (let i = 0; i < 20; i++) {
+      const id = randomInteger(0, 1084);
+      const newItem: SimpleItem = {
+        id: id,
+        name: "Item",
+        img: `https://picsum.photos/id/${id}/200.webp`,
+        tag: randomInteger(0, 1084) % 2 == 0 ? ["favorite"] : [],
+        type: randomInteger(0, 1084) % 3,
+      };
+      returnList.push(newItem);
+    }
+
+    return returnList;
+  }
 
   return {
     session: session.data,
@@ -20,67 +42,18 @@ export async function loader({request}: LoaderFunctionArgs) {
       numOfLikes: 107,
       numOfRatings: 26,
       preferences: [
-        {
-          name: "Music",
-          values: [
-            "Pop",
-            "Jazz",
-            "Classical",
-            "Indie",
-            "Movie-related",
-            "Indian Classical",
-          ],
-        },
-        {name: "Books", values: ["Dystopian", "Non-fiction"]},
-        {name: "Movies", values: ["Thriller", "Horror"]},
+        "Pop",
+        "Jazz",
+        "Classical",
+        "Indie",
+        "Movie-related",
+        "Indian Classical",
+        "Dystopian",
+        "Non-fiction",
+        "Thriller",
+        "Horror",
       ],
-      HistoryItems: [
-        {
-          thumbnailUrl: "https://picsum.photos/200.webp",
-          placeholder: "Item",
-        },
-        {
-          thumbnailUrl: "https://picsum.photos/200.webp",
-          placeholder: "Item",
-        },
-        {
-          thumbnailUrl: "https://picsum.photos/200.webp",
-          placeholder: "Item",
-        },
-        {
-          thumbnailUrl: "https://picsum.photos/200.webp",
-          placeholder: "Item",
-        },
-        {
-          thumbnailUrl: "https://picsum.photos/200.webp",
-          placeholder: "Item",
-        },
-        {
-          thumbnailUrl: "https://picsum.photos/200.webp",
-          placeholder: "Item",
-        },
-        {
-          thumbnailUrl: "https://picsum.photos/200.webp",
-          placeholder: "Item",
-        },
-        {
-          thumbnailUrl: "https://picsum.photos/200.webp",
-          placeholder: "Item",
-          showHeart: true,
-        },
-        {
-          thumbnailUrl: "https://picsum.photos/200.webp",
-          placeholder: "Item",
-        },
-        {
-          thumbnailUrl: "https://picsum.photos/200.webp",
-          placeholder: "Item",
-        },
-        {
-          thumbnailUrl: "https://picsum.photos/200.webp",
-          placeholder: "Item",
-        },
-      ],
+      HistoryItems: makeItems(),
     },
   };
 }
@@ -89,24 +62,25 @@ export default function tab_index(): React.JSX.Element {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const {session, user} = useLoaderData<typeof loader>();
 
-  const colors = ["neutral", "primary", "secondary"];
+  const colors = ["accent", "primary", "secondary"];
   return (
     <>
       <div className="hero min-h-screen">
         {session.userId !== undefined && session.userId !== null && (
-          <div className="hero-content  max-lg:m-12 max-lg:flex-col lg:m-0 lg:flex-row lg:items-end lg:justify-end">
+          <div className="hero-content max-lg:m-0 max-lg:flex-col max-md:w-96 lg:m-0 lg:flex-row lg:items-end lg:justify-end">
             <UserProfileCard user={user} />
 
             <div className="card bg-base-200 shadow-xl">
               <div className="card-body">
-                <h2 className="card-title mx-2 text-3xl">Preferences</h2>
-                <div className="flex flex-row flex-wrap justify-around overflow-x-auto">
-                  <TagList
-                    tag={user.preferences}
-                    colors={colors}
-                    buttonType="none"
-                  />
-                </div>
+                <h2 className="card-title mx-2 text-2xl lg:text-3xl">
+                  Preferences
+                </h2>
+
+                <TagList
+                  tag={user.preferences}
+                  colors={colors}
+                  buttonType="none"
+                />
               </div>
 
               <HistoryItemList title="View History" items={user.HistoryItems} />
