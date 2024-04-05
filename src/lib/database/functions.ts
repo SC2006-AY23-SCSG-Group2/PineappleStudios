3da023e34beba1c26602ce34492d3014196fcf28
Iqoyz <chanyonqi@hotmail.com>
2024-04-06 00:23:37 +0800

    [update] <prisma> : modify prisma.schema and related function

--- src/lib/database/functions.ts
+++ src/lib/database/functions.ts
@@ -19,6 +19,7 @@ function randomInteger(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+export async function getUserInfoByUserId(userId: number) {}
 export async function getItemInfoByItemId(
   itemId: number,
   userId: any,
---
+++ src/lib/database/preference.ts
@@ -0,0 +1,48 @@
+import {prismaClient} from "./prisma";
+
+// create preference
+export const createPreferenceForUser = async (name: string, userId: number) => {
+  try {
+    const preference = await prismaClient.preference.create({
+      data: {
+        name: name,
+        userId: userId,
+      },
+    });
+    return preference;
+  } catch (e) {
+    console.error("Error occurred while creating preference for User:", e);
+  }
+};
+
+//get preference using name and userId
+export const getPreference = async (userId: number, preferenceName: string) => {
+  try {
+    const preference = await prismaClient.preference.findFirst({
+      where: {
+        name: preferenceName,
+        userId: userId,
+      },
+    });
+    return preference;
+  } catch (e) {
+    console.error("Error occured while getting preference : ", e);
+  }
+};
+
+//delete a preference for a user
+export const deletePreference = async (
+  userId: number,
+  preferenceName: string,
+) => {
+  try {
+    return await prismaClient.preference.deleteMany({
+      where: {
+        name: preferenceName,
+        userId: userId,
+      },
+    });
+  } catch (e) {
+    console.error("Error occured while deleting preference : ", e);
+  }
+};
