2def68ef0c9d810a3c828e3abe6e5a0630590096
BobH <dev@mail.bobh.cool>
2024-04-09 00:59:02 +0900

    <update>[HistoryItemList]: improve layout

--- src/app/routes/tab.4/components/HistoryItemList.tsx
+++ src/app/routes/tab.4/components/HistoryItemList.tsx
@@ -15,7 +15,7 @@ export const HistoryItemList: React.FC<ItemsListProps> = ({
   return (
     <div className="card w-full">
       <div className="card-body">
-        <h2 className="card-title mx-6 my-4 text-2xl lg:text-3xl">{title}</h2>
+        <h2 className="card-title mx-2 text-2xl lg:text-3xl">{title}</h2>
         <div className="m-6 grid grid-cols-4 gap-1 max-md:m-0 max-md:grid-cols-3 lg:grid-cols-3">
           {items.map((item, index) => (
             <div key={index} className="my-2 lg:my-4">
