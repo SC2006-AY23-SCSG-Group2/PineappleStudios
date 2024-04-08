b5c832f329b5b5b3b4d87d70bdc926dd7717d911
BobH <dev@mail.bobh.cool>
2024-04-08 16:29:39 +0900

    <chore>[route_naming]: clean naming.

--- src/app/routes/logout/route.tsx
+++ src/app/routes/logout/route.tsx
@@ -16,7 +16,7 @@ export async function loader({request}: LoaderFunctionArgs) {
     session.set("startTime", endTime);
   }
 
-  await destroySession(session);
+  // await destroySession(session);
   return redirect("/login", {
     headers: {
       "Set-Cookie": await destroySession(session),
