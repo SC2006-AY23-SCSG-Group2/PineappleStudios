69248315cd0be1068bfdac4ad6fc6eb38dd55b02
BobH <dev@mail.bobh.cool>
2024-04-08 18:27:00 +0900

    <update>[tab.1]: update tab.1, connected.

--- src/app/routes/tab.1/route.tsx
+++ src/app/routes/tab.1/route.tsx
@@ -1,10 +1,11 @@
 import {LoaderFunctionArgs, json, redirect} from "@remix-run/node";
-import {useLoaderData} from "@remix-run/react";
+import {NavLink, useLoaderData} from "@remix-run/react";
 import React from "react";
 
 import {getLibraryInfoByUserId} from "../../../lib/dataRetrieve/getLibraryInfo";
 import {ItemType, Library, SimpleItem} from "../../../lib/interfaces";
 import {commitSession, destroySession, getSession} from "../../session";
+import {ItemFolderList} from "../_components/ItemFolderList";
 import {ItemList} from "../_components/ItemList";
 
 export async function loader({request}: LoaderFunctionArgs) {
@@ -44,7 +45,9 @@ export async function loader({request}: LoaderFunctionArgs) {
     error: undefined,
   };
 
-  if (library) {
+  console.log(library);
+
+  if (!library) {
     session.flash("error", "Library Cannot found");
 
     jsonData = {
@@ -81,6 +84,8 @@ export default function tab_index(): React.JSX.Element {
     );
   }
 
+  loaderData.data.items.map((e) => console.log(e));
+
   const favoriteItems: SimpleItem[] = loaderData.data.items.filter(
     (item: SimpleItem) =>
       item.tag.includes("favorite") || item.tag.includes("favourite"),
@@ -92,12 +97,31 @@ export default function tab_index(): React.JSX.Element {
 
   return (
     <>
-      <ItemList title="Favorites" items={favoriteItems} />
+      {favoriteItems.length !== 0 && (
+        <>
+          <div className="divider"></div>
+          <ItemList title="Favorites" items={favoriteItems} />
+        </>
+      )}
+
+      {loaderData.data.folders.length !== 0 && (
+        <>
+          <div className="divider"></div>
+          <ItemFolderList title="Folders" items={loaderData.data.folders} />
+        </>
+      )}
+
+      {loaderData.data.series.length !== 0 && (
+        <>
+          <div className="divider"></div>
+          <ItemFolderList title="Series" items={loaderData.data.series} />
+        </>
+      )}
 
       {notFavoriteItems.filter((x: SimpleItem) => x.type === ItemType.Song)
         .length !== 0 && (
         <>
-          <div className="divider"></div>
+          <div className="divider m-4"></div>
           <ItemList
             title="Music"
             items={notFavoriteItems.filter(
@@ -146,7 +170,26 @@ export default function tab_index(): React.JSX.Element {
           />
         </>
       )}
-      <div className="divider m-4 p-4"></div>
+
+      {loaderData.data.items.length +
+        loaderData.data.folders.length +
+        loaderData.data.series.length ===
+        0 && (
+        <>
+          <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+              <div className="max-w-md">
+                <h1 className="text-3xl font-bold text-error">
+                  There is no item in your library yet, browser more.
+                </h1>
+                <NavLink className="btn btn-primary" to="/tab/2">
+                  Browser More
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
