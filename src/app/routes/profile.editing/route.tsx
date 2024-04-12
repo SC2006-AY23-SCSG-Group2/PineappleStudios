86aa779e556164ba150dae2ff119e111e2a5c6f9
Iqoyz <chanyonqi@hotmail.com>
2024-04-12 11:52:06 +0800

    [update] : <profile> : preferences page, it it not done yet, frontend team please check

--- src/app/routes/profile.editing/route.tsx
+++ src/app/routes/profile.editing/route.tsx
@@ -1,49 +1,28 @@
+import {LoaderFunctionArgs, redirect} from "@remix-run/node";
+import {Outlet} from "@remix-run/react";
 import React from "react";
+
+import {destroySession, getSession} from "../../session";
+
+export async function loader({request}: LoaderFunctionArgs) {
+  const session = await getSession(request.headers.get("cookie"));
+
+  if (!session.has("userId")) {
+    session.flash("error", "User not login");
+
+    return redirect("/login", {
+      headers: {
+        "Set-Cookie": await destroySession(session),
+      },
+    });
+  }
+  return session.data;
+}
 
 export default function tab_index(): React.JSX.Element {
   return (
     <>
-      <p>aaaa</p>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
-      <p>aaaa</p>
+      <Outlet />
     </>
   );
 }
