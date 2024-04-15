09715d71e8c0f863dcae2538d530aea84f8d0308
BobH <dev@mail.bobh.cool>
2024-04-16 01:46:54 +0900

    <update>[historyItemList]: add info hover

--- src/app/routes/_components/HistoryItemList.tsx
+++ src/app/routes/_components/HistoryItemList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import {SimpleItem} from "../../../lib/interfaces";
+import InfoHover from "./InfoHover";
 import {ItemSmallCard} from "./ItemSmallCard";
 
 interface ItemsListProps {
@@ -15,7 +16,10 @@ export const HistoryItemList: React.FC<ItemsListProps> = ({
   return (
     <div className="card w-full shadow-none">
       <div className="card-body">
-        <h2 className="card-title mx-2 text-2xl lg:text-3xl">{title}</h2>
+        <h2 className="card-title mx-2 text-2xl lg:text-3xl">
+          {title}
+          <InfoHover info={"This is your " + title.toLowerCase()} />
+        </h2>
         <div className="m-6 grid grid-cols-4 gap-1 max-md:m-0 max-md:grid-cols-3 lg:m-0 lg:grid-cols-3 xl:grid-cols-3">
           {items.map((item, index) => (
             <div key={index} className="my-2 lg:my-4">
