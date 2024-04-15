776b0b947e39139f9c180474bc5a52fdb8ffceb6
BobH <dev@mail.bobh.cool>
2024-04-16 01:32:45 +0900

    <update>[HistoryItemList]: update layout when screen=lg

--- src/app/routes/_components/HistoryItemList.tsx
+++ src/app/routes/_components/HistoryItemList.tsx
@@ -16,7 +16,7 @@ export const HistoryItemList: React.FC<ItemsListProps> = ({
     <div className="card w-full shadow-none">
       <div className="card-body">
         <h2 className="card-title mx-2 text-2xl lg:text-3xl">{title}</h2>
-        <div className="m-6 grid grid-cols-4 gap-1 max-md:m-0 max-md:grid-cols-3 lg:grid-cols-3">
+        <div className="m-6 grid grid-cols-4 gap-1 max-md:m-0 max-md:grid-cols-3 lg:m-0 lg:grid-cols-3 xl:grid-cols-3">
           {items.map((item, index) => (
             <div key={index} className="my-2 lg:my-4">
               <ItemSmallCard data={item} />
